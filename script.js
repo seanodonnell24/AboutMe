1900506a6fa9ce9ba3146e870639a7682413c1d1
seanodonnell24 <143746100+seanodonnell24@users.noreply.github.com>
2023-08-31 14:30:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function scrollOnClick1() {
+  scroll(0, 200);
+}
+
+function scrollOnClick2() {
+  scroll(0, 100);
+}
+
+function scrollOnClick3() {
+  scroll(0, 100);
+}
+
+function scrollOnClick4() {
+  scroll(0, 100);
+}
