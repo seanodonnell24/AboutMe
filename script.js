4cd69f1a1da3bce49b60fbcb11589e7efb4181ee
seanodonnell24 <143746100+seanodonnell24@users.noreply.github.com>
2023-08-31 22:53:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function scrollOnClick1() {
-  scroll(0, 200);
+  scroll(0, 800);
 }
 
 function scrollOnClick2() {
-  scroll(0, 100);
+  scroll(0, 2000);
 }
 
 function scrollOnClick3() {
-  scroll(0, 100);
+  scroll(0, 3150);
 }
 
 function scrollOnClick4() {
-  scroll(0, 100);
+  scroll(0, 6000);
 }
